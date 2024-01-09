dccc31e75932a773d5b05a3218a6a252cc0888aa
Eric Luo <ericzyluo2002@gmail.com>
2024-01-08 21:35:27 -0500

    Created account model

--- mtgguesser/guesserapp/models.py
+++ mtgguesser/guesserapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Account(models.Model):
+    username = models.CharField("Username", max_length=256)
+    email = models.EmailField()
+    total_guesses = models.IntegerField()
+    total_correct = models.IntegerField()
+    most_popular = models.JSONField()
+    extra_stats = models.JSONField()
