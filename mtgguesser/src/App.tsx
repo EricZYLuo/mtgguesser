b3559d2031ffc0eef553ddbe779601b0106c4d41
Eric Luo <ericzyluo2002@gmail.com>
2024-10-15 20:02:27 -0400

    Added card format changer and function for api calls

--- mtgguesser/src/App.tsx
+++ mtgguesser/src/App.tsx
@@ -1,6 +1,6 @@
 
 
-import { SyntheticEvent, useState } from 'react';
+import { SyntheticEvent, useState, useEffect } from 'react';
 import './App.css'
 
 import { StatsDisplay } from './StatsDisplay';
@@ -10,30 +10,64 @@ function App() {
 
   const [score, setScore] = useState(0); 
   const [text, setText] = useState("");
+  const [cardpool, setPool] = useState("all");
+
+
+
 
   const key = "abc";
 
-  function handleSubmit(e: SyntheticEvent) { 
-    e.preventDefault();
-    if(text === key) {
-      setScore(score + 1);
+  function handleChangeType(e:SyntheticEvent) {
+    setPool(e.target.value);
+  }
+
+  function getCardData() {
+    const searchLinkBase = "https://api.scryfall.com/cards/random";
+    let searchLinkEnd = "";
+    if(cardpool === "all") {
+      searchLinkEnd = "";
+    } 
+    else if (cardpool === "standard") {
+      searchLinkEnd = "f%3Astandard";
     }
-    else {
-      console.log(text);
+    else if (cardpool === "modern") {
+      searchLinkEnd = "f%3Amodern";
     }
-    
+
+    fetch(searchLinkBase+searchLinkEnd).then(
+      response => response.json())
+      .then(
+        json => {
+          console.log(json);
+          console.log(json.name);
+          console.log(json.set_name);
+          console.log(json.type_line);
+          console.log(json.image_uris.art_crop);
+        }
+      )
+      .catch(error => console.error(error));
+
   }
 
   return (
     <>
       <div>
         <h1>MTG Guesser</h1>
+        <label for="typeSelect">Choose Type</label>
+        <select id="typeSelect" value={cardpool} onChange={handleChangeType}>
+          <option value="all">All Cards</option>
+          <option value="standard">Standard</option>
+          <option value="modern">Modern</option>
+        </select>
         <StatsDisplay score={score}></StatsDisplay>
         <div>
 
         </div>
 
         <GuessInput text={text} score={score} answer={key} setText={setText} setScore={setScore}></GuessInput>
+
+        <button onClick={getCardData}>Test</button>
+
       </div>
 
     </>
