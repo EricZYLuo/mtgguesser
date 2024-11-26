6d84a2080881a3acc9389f8bffe413cdf9c32214
Eric Luo <ericzyluo2002@gmail.com>
2024-11-26 17:10:52 -0500

    Added edit distance algorithm to allow close enough answers

--- mtgguesser/src/GuessInput.tsx
+++ mtgguesser/src/GuessInput.tsx
@@ -33,12 +33,46 @@ export function GuessInput({guessType, score, data, setScore, completion, setCom
         answer = data.type_line;
     }
 
+
+    function editDistance(string1: string, string2: string): number {
+
+        let dparr: Array<number[]> = [];
+
+        for(let i = 0; i < string1.length + 1; i++) {
+            dparr.push([]);
+            for(let j = 0; j < string2.length + 1; j++) {
+                if(i === 0) {dparr[i].push(j);}
+                if(j === 0) {dparr[i].push(i);}
+                else {dparr[i].push(-1);}
+            }
+        }
+
+        let string1Arr = [...string1];
+        let string2Arr = [...string2];
+
+        for(let i = 1; i < string1.length + 1; i++) {
+            for(let j = 1; j < string2.length + 1; j++) {
+                dparr[i][j] = Math.min(dparr[i-1][j] + 1, dparr[i][j-1] + 1, string1Arr[i-1] === string2Arr[j-1] ? dparr[i-1][j-1] : dparr[i-1][j-1]);
+            }
+        }
+        return dparr[string1.length][string2.length];
+
+    } 
+
+    function matchVals(input: string, checkVal: string): boolean {
+        let editDist: number = editDistance(input.toLowerCase(), checkVal.toLowerCase());
+        let closeEnough: boolean = checkVal.length > 10 ? editDist < 3 : editDist < 2;
+        console.log(input.toLowerCase());
+        console.log(checkVal.toLowerCase());
+        return input.toLowerCase() === checkVal.toLowerCase() || closeEnough;
+    }
+
     function checkSol(): boolean {
         if(guessType === "set" && data) {
-            return text === data.set || text === answer;
+            return text === data.set || matchVals(text, answer);
         }
         else {
-            return text === answer;
+            return matchVals(text, answer);
         }
         return false;
     }
